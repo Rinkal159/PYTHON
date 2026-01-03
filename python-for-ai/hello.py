7e2260cc4d2c89c045dacaaba902efe9a4555318
rinkal159 <rinkalsingapuri@gmail.com>
2026-01-03 11:56:07 +0530

    Added string methods and division clarification.

--- python-for-ai/hello.py
+++ python-for-ai/hello.py
@@ -1,20 +1,62 @@
-print("Hello world!")
+print("Hello world!") # single line print statement
+print("""hii
+im am rinkal""") # multi line print statement
+
+#&------------------------------------------------------------------------------------------------------
 
 # single line comment
 """
-double line comment
-"""
+..
+..
+..
+""" # multi line comment
+
+#&------------------------------------------------------------------------------------------------------
 
 # snake case is for variable declartion in python. eg. user_input, my_age, user_password
 # camel case is for variable declartion in java. eg. userInput, myAge, userPassword
 
-str = "rinkal" #single line string
-str = """ hi
+#&------------------------------------------------------------------------------------------------------
+
+str = "rinkal" # single line string
+str1 = """ hi
 I 
 Am
 Rinkal
 !!!
-""" #Multi line string
+""" # Multi line string
 
-multiply = "rinkal " * 5 #result: rinkal rinkal rinkal rinkal rinkal
+#&------------------------------------------------------------------------------------------------------
 
+#* String methods
+
+#Strings in Python are immutable
+
+print()
+print('STRING METHODS')
+print("--------------")
+
+str = "I am Rinkal"
+print(str * 5)
+print(len(str))
+print(str.startswith('i'))
+print(str.endswith('al'))
+print(str.upper())
+print(str.lower())
+print(str.title()) # title() capitalizes first character of each word
+print(str.capitalize()) # capitalize() capitalizes only first character.
+
+print()
+
+#&------------------------------------------------------------------------------------------------------
+
+# In python, divison always returns float.
+simple_div = 10/3
+floor_div = 10//3
+
+#&------------------------------------------------------------------------------------------------------
+
+# f-strings
+name="Rinkal"
+age=18
+print(f"{name} is {age} years old.")
