343e0efffa0f39cf056fabd5bf9abfa456933617
rinkal159 <rinkalsingapuri@gmail.com>
2026-01-03 10:52:15 +0530

    Understanding of comments, variable declarations, single and multi line string.

--- python-for-ai/hello.py
+++ python-for-ai/hello.py
@@ -1,1 +1,20 @@
 print("Hello world!")
+
+# single line comment
+"""
+double line comment
+"""
+
+# snake case is for variable declartion in python. eg. user_input, my_age, user_password
+# camel case is for variable declartion in java. eg. userInput, myAge, userPassword
+
+str = "rinkal" #single line string
+str = """ hi
+I 
+Am
+Rinkal
+!!!
+""" #Multi line string
+
+multiply = "rinkal " * 5 #result: rinkal rinkal rinkal rinkal rinkal
+
