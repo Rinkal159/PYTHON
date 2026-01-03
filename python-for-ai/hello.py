283e77f4cc71dde90b50f2abb091c41ec03cf20c
rinkal159 <rinkalsingapuri@gmail.com>
2026-01-03 15:37:19 +0530

    Give heading to sections, adds more string methods, and makes the concept clear of logical operators and if-elif-else.

--- python-for-ai/hello.py
+++ python-for-ai/hello.py
@@ -1,8 +1,13 @@
+
+#* print statement
+
 print("Hello world!") # single line print statement
 print("""hii
 im am rinkal""") # multi line print statement
 
 #&------------------------------------------------------------------------------------------------------
+
+#* Comments
 
 # single line comment
 """
@@ -13,10 +18,14 @@ im am rinkal""") # multi line print statement
 
 #&------------------------------------------------------------------------------------------------------
 
+#* variable declaration
+
 # snake case is for variable declartion in python. eg. user_input, my_age, user_password
 # camel case is for variable declartion in java. eg. userInput, myAge, userPassword
 
 #&------------------------------------------------------------------------------------------------------
+
+#* string
 
 str = "rinkal" # single line string
 str1 = """ hi
@@ -45,18 +54,56 @@ print(str.upper())
 print(str.lower())
 print(str.title()) # title() capitalizes first character of each word
 print(str.capitalize()) # capitalize() capitalizes only first character.
+print(str.swapcase())
+print(str.find("a")) # indexOf() in javascript, if word is not present it returns -1
+print(str.index("a")) # same as find(), but if word is not present then it raises an error
+print(str.count("I"))
+print(str.strip())
+print(str.replace("Rinkal", "Rinku"))
 
 print()
 
 #&------------------------------------------------------------------------------------------------------
 
+#* divion in python
+
 # In python, divison always returns float.
+
 simple_div = 10/3
 floor_div = 10//3
 
 #&------------------------------------------------------------------------------------------------------
 
-# f-strings
+#* f-strings
+
 name="Rinkal"
 age=18
 print(f"{name} is {age} years old.")
+
+#&------------------------------------------------------------------------------------------------------
+
+#* Logical operators
+
+age = 20
+
+if(age>18 and age>25):
+    print("Logical and: Both are correct that's why statement is printed.")
+    
+if(age>18 or age>25):
+    print("Logical or: Any one condition is satisfied that's why statement is printed.")
+    
+if(not age>30):
+    print("Logical not: if opposite of condition is satisfied, that's why statement is printed")
+    
+#&------------------------------------------------------------------------------------------------------
+
+#* if-elif-else
+
+need_coffee = input("Wanna some coffee? : ")
+
+if(need_coffee.lower()=="yes"):
+    print("Absoulutly!! grab your favourite coffee.")
+elif(need_coffee.lower()=="no"):
+    print("Oh! That's fine.")
+else:
+    print("please say \"yes\" or \"no\"")   
