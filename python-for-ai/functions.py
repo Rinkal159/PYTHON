2b12ede71a85c7ac19a8a18dfe6eb8c033b8d01b
rinkal159 <rinkalsingapuri@gmail.com>
2026-01-08 22:08:36 +0530

    Introduces functions in python.

---
+++ python-for-ai/functions.py
@@ -0,0 +1,16 @@
+def greet_me(name):
+    print(f"Hello, {name}")
+
+greet_me(input("What is your name? : "))
+
+#&---------------------------------------
+
+# counter = 0
+
+# def increase_counter(counter):
+#     return counter+1
+    
+# counter = increase_counter(counter)
+# counter = increase_counter(counter)
+
+# print(counter)
